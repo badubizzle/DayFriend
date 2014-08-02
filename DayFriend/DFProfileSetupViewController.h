0900b36b6c1f0f230310cf1cbdf954034a5c5f50
Spencer Yen <spencersyen@gmail.com>
2014-08-02 00:52:40 -0700

    added classes and segues

---
+++ DayFriend/DFProfileSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  DFProfileSetupViewController.h
+//  DayFriend
+//
+//  Created by Spencer Yen on 8/2/14.
+//  Copyright (c) 2014 Spencer Yen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFProfileSetupViewController : UIViewController
+
+@end
